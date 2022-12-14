9f620a1b5d98c4269b788012f1697d99d09f0005
tabrezid54 <103884267+tabrezid54@users.noreply.github.com>
2022-12-15 02:46:50 +0530

    Update InsertData.sql

--- mysql-image/sql-scripts/InsertData.sql
+++ mysql-image/sql-scripts/InsertData.sql
@@ -1,10 +1,8 @@
-INSERT INTO Employee (first_name) VALUES
-     ('admin'),('user1'),('user2'),('user3');
-INSERT INTO Employee (last_name) VALUES
-     ('admin'),('user1'),('user2'),('user3');
-INSERT INTO Employee (email) VALUES
-     ('admin@gmail'),('user1@gmail'),('user2@gmail'),('user3@gmail');
-INSERT INTO Employee (username) VALUES
-     ('admin'),('user1'),('user2'),('user3');
-INSERT INTO Employee (password) VALUES
-     ('admin'),('user1'),('user2'),('user3');
+INSERT INTO Employee (first_name, last_name, email, username, password) 
+VALUES ('admin', 'admin', 'admin@gmail', 'admin', 'admin');
+INSERT INTO Employee (first_name, last_name, email, username, password) 
+VALUES ('user1', 'user1', 'user1@gmail', 'user1', 'user1');
+INSERT INTO Employee (first_name, last_name, email, username, password) 
+VALUES ('user2', 'user2', 'user2@gmail', 'user2', 'user2');
+INSERT INTO Employee (first_name, last_name, email, username, password) 
+VALUES ('user3', 'user3', 'user3@gmail', 'user3', 'user3');
