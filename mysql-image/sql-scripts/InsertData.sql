411a0fbf373561ec717a73b6d169d0212afa3e98
tabrezid54 <103884267+tabrezid54@users.noreply.github.com>
2022-12-14 19:20:36 +0530

    Create InsertData.sql

---
+++ mysql-image/sql-scripts/InsertData.sql
@@ -0,0 +1,5 @@
+INSERT INTO Employee (first_name, last_name, email, username, password) 
+VALUES ('admin', 'admin', 'admin@gamil.com', 'admin'),
+    ('user1', 'user1', 'user1@gamil.com', 'user1'), 
+    ('user2', 'user2', 'user2@gamil.com', 'user2'), 
+    ('user3', 'user3', 'user3@gamil.com', 'user3');
