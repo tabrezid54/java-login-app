40eb9a6962b275123963ae81b412667380b00274
tabrezid54 <103884267+tabrezid54@users.noreply.github.com>
2022-12-14 18:17:28 +0530

    Create CreateTable.sql

---
+++ mysql-image/sql-scripts/CreateTable.sql
@@ -0,0 +1,1 @@
+CREATE TABLE Employee ( id int unsigned auto_increment not null, first_name varchar(250), last_name varchar(250), email varchar(250), username varchar(250), password varchar(250), regdate timestamp, primary key (id) );
